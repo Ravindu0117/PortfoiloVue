243ac69eb154545e76782d116725da42ea2963b4
Ravindu Samarasinghe <124066949+Ravindu0117@users.noreply.github.com>
2026-01-20 11:41:05 +0530

    add base: '/ '

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/'
   resolve: {
     alias: {
       // This tells Vite that @ starts at the /src folder
